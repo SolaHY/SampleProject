f3333073f65c924804c3f8783a615b3ab20d363d
SolaHY <bigsky0111@gmail.com>
2023-03-17 17:41:18 +0900

    mergeTest

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,16 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+<<<<<<< Updated upstream
 require __DIR__.'/auth.php';
 
 Route::get('/list', 'App\Http\Controllers\BooklistController@showList')->name('list');
+=======
+//本一覧
+Route::get('/products', 'ProductController@index')->name('products.index');
+
+// 写真一覧
+Route::get('/photos', 'PhotoController@index')->name('photos.index');
+
+require __DIR__ . '/auth.php';
+>>>>>>> Stashed changes
